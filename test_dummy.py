c57ec7e94c55b17e4de718f5c0ea4bae6e276bf2
Kaustubh Srikantapuram <kaustubhsrik@gmail.com>
2026-06-01 22:38:03 -0700

    Fixed comments

--- test_dummy.py
+++ test_dummy.py
@@ -1,6 +1,6 @@
 from tests.conftest import fixture_db  # noqa: F401  # pyright: ignore
 from tests.test_pdf_pipeline import TestCreditLoadVariations, default_config, pdf_dir  # noqa: F401, E501  # pyright: ignore
-
+# ignore error lines
 
 def test_manual(pdf_dir, fixture_db, default_config):  # noqa: F811
     t = TestCreditLoadVariations()
